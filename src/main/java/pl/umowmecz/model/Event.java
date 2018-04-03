fa54a66b46865d232b1bbabe591c5cb2e5560071
Adrian Dyjecinski <adriandyjecinski@gmail.com>
2018-04-03 19:48:21 +0200

    Cleanup in templates and entites

--- src/main/java/pl/umowmecz/model/Event.java
+++ src/main/java/pl/umowmecz/model/Event.java
@@ -14,7 +14,7 @@ public class Event implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String title;
     private String description;
     private Date postedAt;
@@ -34,11 +34,11 @@ public class Event implements Serializable {
         this.type = type;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
