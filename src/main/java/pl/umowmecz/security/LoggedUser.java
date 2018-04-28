48b127342d2be504893e7245b36a782a7822b417
Adrian Dyjecinski <adriandyjecinski@gmail.com>
2018-04-28 16:30:49 +0200

    LoggedUser null exception fixed

--- src/main/java/pl/umowmecz/security/LoggedUser.java
+++ src/main/java/pl/umowmecz/security/LoggedUser.java
@@ -2,13 +2,19 @@ package pl.umowmecz.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import pl.umowmecz.model.User;
 import pl.umowmecz.repository.UserRepository;
 
+@Component
 public class LoggedUser {
+    
+    private static UserRepository userRepository;
 
     @Autowired
-    private static UserRepository userRepository;
+    public LoggedUser(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public static User getLoggedUser() {
         org.springframework.security.core.userdetails.User userDetails =
